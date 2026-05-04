5aab97fce6ef714e7609a894d14e7627fc6b949b
Evgeny <vaazhen@mail.ru>
2026-05-04 12:59:49 +0300

    perf(crypto): batch device and prekey lookups

--- backend/src/main/java/ru/messenger/chaosmessenger/crypto/prekey/SignedPreKeyRepository.java
+++ backend/src/main/java/ru/messenger/chaosmessenger/crypto/prekey/SignedPreKeyRepository.java
@@ -2,11 +2,23 @@ package ru.messenger.chaosmessenger.crypto.prekey;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface SignedPreKeyRepository extends JpaRepository<SignedPreKey, Long> {
 
     Optional<SignedPreKey> findTopByDeviceIdOrderByCreatedAtDesc(Long deviceId);
+
+    @org.springframework.data.jpa.repository.Query(value = """
+            select distinct on (device_db_id) *
+            from signed_prekeys
+            where device_db_id in (:deviceIds)
+            order by device_db_id, created_at desc, id desc
+            """, nativeQuery = true)
+    List<SignedPreKey> findLatestByDeviceIds(
+            @org.springframework.data.repository.query.Param("deviceIds") Collection<Long> deviceIds
+    );
 
     Optional<SignedPreKey> findByDeviceIdAndPreKeyId(Long deviceId, Integer preKeyId);
 
